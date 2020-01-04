c4956b009d61d90cd7e755dcc62926cda88d8331
evbroom <etbptd@gmail.com>
2020-01-04 16:00:40 -0500

    Create grid

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.getElementById("sketch-container");
+
+function makeRows(rows, columns) {
+	container.style.setProperty('--grid-rows', rows);
+	container.style.setProperty('--grid-cols', columns);
+
+	for (i = 0; i < (rows * columns); i++) {
+		let cell = document.createElement('div');
+		// cell.innerText = (i + 1);
+		container.appendChild(cell).className = 'grid-item';
+	};
+};
+
+makeRows(16, 16);
