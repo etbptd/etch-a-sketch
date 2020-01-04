45987d7bd25dd7075e8d930766e2211d88a27e06
evbroom <etbptd@gmail.com>
2020-01-04 16:40:17 -0500

    Add cell hover and clear functionality

--- script.js
+++ script.js
@@ -11,4 +11,18 @@ function makeRows(rows, columns) {
 	};
 };
 
-makeRows(16, 16);
+makeRows(10, 10);
+
+function colorChange(e) {
+	this.classList.add('sketched');
+}
+
+const cells = document.querySelectorAll('.grid-item');
+cells.forEach(cell => cell.addEventListener('mouseover', colorChange));
+
+const clearButton = document.querySelector('.clear-button');
+clearButton.addEventListener('click', (e) => {
+	cells.forEach(cell => cell.classList.remove('sketched'));
+})
+
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
